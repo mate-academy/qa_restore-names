67dc5d317f5a34af6ce957d23c263e1c9519dd00
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2022-02-01 15:07:21 +0200

    final

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,17 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
+  const names = [{ fullName: 'James Smith' }];
 
-  it('should ', () => {
-
+  it('should be a function', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should return "James" when full name is equal to "James Smith"', () => {
+    restoreNames(names);
+
+    expect(names[0].firstName)
+      .toBe('James');
+  });
 });
