e35703823a80e21e3eaf8273740fe5b18ddca364
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2022-02-02 16:48:03 +0200

    fixedSolution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -2,16 +2,40 @@
 
 describe('restoreNames', () => {
   const { restoreNames } = require('./restoreNames');
-  const names = [{ fullName: 'James Smith' }];
+  const names = [
+    { fullName: 'James Smith' },
+    {
+      firstName: 'Rob',
+      fullName: 'Robert Scott',
+    },
+    {
+      firstName: undefined,
+      fullName: 'Andre Robson',
+    },
+  ];
 
   it('should be a function', () => {
     expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  it('should return "James" when full name is equal to "James Smith"', () => {
+  it(`should create properties 'fullName' if it is missing`, () => {
     restoreNames(names);
 
     expect(names[0].firstName)
       .toBe('James');
+  });
+
+  it('should not change the name if it exists', () => {
+    restoreNames(names);
+
+    expect(names[1].firstName)
+      .toBe('Rob');
+  });
+
+  it(`should overwrite the name if it is 'undefined'`, () => {
+    restoreNames(names);
+
+    expect(names[2].firstName)
+      .toBe('Andre');
   });
 });
