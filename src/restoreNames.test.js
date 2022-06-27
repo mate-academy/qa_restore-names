c5e26ef4feb9b2d93aeaacf9aa7805d23a851986
ctra <vadym.tsymbal.ua@gmail.com>
2022-06-27 14:59:37 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -10,58 +10,54 @@ describe('restoreNames', () => {
 
   it(`should modify objects where field 'firstName' is undefined`, () => {
     const testUsers = [
-      { firstName: undefined, lastName: 'House', fullName: 'Gregory House' },
+      { firstName: undefined, lastName: 'Holy', fullName: 'Jack Holy' },
     ];
 
     restoreNames(testUsers);
 
     expect(testUsers[0])
-      .toEqual({ firstName: 'Gregory', lastName: 'House', fullName: 'Gregory House' });
+      .toEqual({ firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy' });
   });
 
   it(`should not modify objects where field 'firstName' isn't empty`, () => {
     const testUsers = [
-      { firstName: 'Loly', lastName: 'S.Oliva', fullName: 'Loly S.Oliva' },
+      { firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy' },
     ];
 
     restoreNames(testUsers);
 
     expect(testUsers[0])
-      .toEqual({ firstName: 'Loly', lastName: 'S.Oliva', fullName: 'Loly S.Oliva' });
+      .toEqual({ firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy' });
   });
 
   it(`should work with an array of objects where all 'firstName' fields are undefined`, () => {
     const testUsers = [
-      { firstName: undefined, lastName: 'House', fullName: 'Gregory House' },
-      { firstName: undefined, lastName: 'Clarkson', fullName: 'Jeremy Clarkson' },
+      { firstName: undefined, lastName: 'Holy', fullName: 'Jack Holy' },
+      { firstName: undefined, lastName: 'Holy', fullName: 'Jack Holy' },
     ];
 
     restoreNames(testUsers);
 
     expect(testUsers)
       .toEqual([
-        { firstName: 'Gregory', lastName: 'House', fullName: 'Gregory House' },
-        { firstName: 'Jeremy', lastName: 'Clarkson', fullName: 'Jeremy Clarkson' },
+        { firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy' },
+        { firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy' },
       ]);
   });
 
   it(`should work with an array of objects where some 'firstName' fields are undefined`, () => {
     const testUsers = [
-      { firstName: undefined, lastName: 'House', fullName: 'Gregory House' },
-      { firstName: undefined, lastName: 'Clarkson', fullName: 'Jeremy Clarkson' },
-      { firstName: 'Loly', lastName: 'S.Oliva', fullName: 'Loly S.Oliva' },
-      { firstName: undefined, lastName: 'Sunderland', fullName: 'Robert Sunderland' },
+      { firstName: undefined, lastName: 'Holy', fullName: 'Jack Holy' },
+      { firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy' },
     ];
 
     restoreNames(testUsers);
 
     expect(testUsers)
       .toEqual([
-        { firstName: 'Gregory', lastName: 'House', fullName: 'Gregory House' },
-        { firstName: 'Jeremy', lastName: 'Clarkson', fullName: 'Jeremy Clarkson' },
-        { firstName: 'Loly', lastName: 'S.Oliva', fullName: 'Loly S.Oliva' },
-        { firstName: 'Robert', lastName: 'Sunderland', fullName: 'Robert Sunderland' },
-      ]);
+        { firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy' },
+        { firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy' },
+    ]);
   });
 
   // write tests here it ( `should return 'Jack Holy' if name is undefined` () => {});
