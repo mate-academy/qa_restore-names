f383c351d504ff239e7520a46effbc007caae916
Dasha Protsko <protscodasha@icloud.com>
2022-08-17 18:00:38 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,56 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`should return nothing`, () => {
+    expect(restoreNames([
+      {
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ]));
+  });
+
+  it(`should add the firstName if it's missed`, () => {
+    const users = [{
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    restoreNames(users);
+
+    const expected = [{
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    expect(users)
+      .toEqual(expected);
+  });
+
+  it(`should add the firstName if t's undefined`, () => {
+    const users = [{
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    restoreNames(users);
+
+    const expected = [{
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    expect(users)
+      .toEqual(expected);
+  });
 });
