3f7c6b7ad1b3eb06244fa3ba985f0d5370e5c6fc
Dasha Protsko <protscodasha@icloud.com>
2022-08-19 12:51:01 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -8,13 +8,32 @@ describe('restoreNames', () => {
       .toBeInstanceOf(Function);
   });
 
-  it(`should return nothing`, () => {
-    expect(restoreNames([
-      {
-        lastName: 'Holy',
-        fullName: 'Jack Holy',
-      },
-    ]));
+  it(`should return an empty array`, () => {
+    const users = [];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual([]);
+  });
+
+  it(`should add the firstName if it's empty`, () => {
+    const users = [{
+      firstName: '',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    restoreNames(users);
+
+    const expected = [{
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    expect(users)
+      .toEqual(expected);
   });
 
   it(`should add the firstName if it's missed`, () => {
@@ -35,9 +54,28 @@ describe('restoreNames', () => {
       .toEqual(expected);
   });
 
-  it(`should add the firstName if t's undefined`, () => {
+  it(`should add the firstName if it's undefined`, () => {
     const users = [{
       firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    restoreNames(users);
+
+    const expected = [{
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    expect(users)
+      .toEqual(expected);
+  });
+
+  it(`should return the object if the firstName exist`, () => {
+    const users = [{
+      firstName: 'Jack',
       lastName: 'Holy',
       fullName: 'Jack Holy',
     }];
