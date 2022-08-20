c94ded5cc718921a6aa6c125c148f04bb55722b0
nicatino <epicjew.nougoi@gmail.com>
2022-08-20 18:20:03 +0300

    Solution

---
+++ src/resroreNameUsers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const users = [
+    {
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+    {
+      firstName: '',
+      lastName: 'Bowie',
+      fullName: 'David Bowie',
+    },
+    {
+      firstName: '',
+      lastName: 'Arestovich',
+      fullName: '',
+    },
+  ];
+
+module.exports = { users };
