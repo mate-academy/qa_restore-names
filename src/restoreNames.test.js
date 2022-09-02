be795b4f68ea43c56cb3484bb37d2dbd3d3cdcda
Ivan Kunitskiy <kunitskivan@gmail.com>
2022-09-02 18:29:34 +0300

    Added tests for restoreNames function!

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,140 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it(`should be declared`, () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`Should add a first name if absent`, () => {
+    const users = [
+      {
+        lastName: 'Seagal',
+        fullName: 'Petro Seagal',
+      },
+      {
+        lastName: 'Stalone',
+        fullName: 'Slavik Stalone',
+      },
+    ];
+
+    const result = [
+      {
+        firstName: 'Petro',
+        lastName: 'Seagal',
+        fullName: 'Petro Seagal',
+      },
+      {
+        firstName: 'Slavik',
+        lastName: 'Stalone',
+        fullName: 'Slavik Stalone',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual(result);
+  });
+
+  it(`Should add a first name if undefined`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Poroshenko',
+        fullName: 'Duncan Poroshenko',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Hopkins',
+        fullName: 'Anton Hopkins',
+      },
+    ];
+
+    const result = [
+      {
+        firstName: 'Duncan',
+        lastName: 'Poroshenko',
+        fullName: 'Duncan Poroshenko',
+      },
+      {
+        firstName: 'Anton',
+        lastName: 'Hopkins',
+        fullName: 'Anton Hopkins',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual(result);
+  });
+
+  it(`Should add a first name if the firstName is 'Null'`, () => {
+    const users = [
+      {
+        firstName: null,
+        lastName: 'Anderson',
+        fullName: 'Polina Anderson',
+      },
+      {
+        firstName: null,
+        lastName: 'Jolie',
+        fullName: 'Angelina Jolie',
+      },
+    ];
+
+    const result = [
+      {
+        firstName: 'Polina',
+        lastName: 'Anderson',
+        fullName: 'Polina Anderson',
+      },
+      {
+        firstName: 'Angelina',
+        lastName: 'Jolie',
+        fullName: 'Angelina Jolie',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual(result);
+  });
+
+  it(`Shouldn't be changed if the first name has correct property`, () => {
+    const users = [
+      {
+        firstName: 'Uma',
+        lastName: 'Thurman',
+        fullName: 'Uma Thurman',
+      },
+      {
+        firstName: 'Gal',
+        lastName: 'Gadot',
+        fullName: 'Gal Gadot',
+      },
+    ];
+
+    const result = [
+      {
+        firstName: 'Uma',
+        lastName: 'Thurman',
+        fullName: 'Uma Thurman',
+      },
+      {
+        firstName: 'Gal',
+        lastName: 'Gadot',
+        fullName: 'Gal Gadot',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual(result);
+  });
 });
