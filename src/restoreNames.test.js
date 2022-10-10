82c075f77d25cabb1bd8e2a8823b80546f386644
Dmytro Semenov <Dmytro.Semenov@tx.local>
2022-10-10 22:31:22 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,73 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  const usersMock = [
+    {
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+  ];
 
+  it('should not return anything', () => {
+    const usersForTesting = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    expect(restoreNames(usersForTesting)).toBeUndefined();
   });
 
-  // write tests here
+  it(`should return the correct value if the`
+    + ` 'firstName' field is missing`, () => {
+    const usersForTesting = [
+      {
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(usersForTesting);
+
+    expect(usersForTesting).toEqual(usersMock);
+  });
+
+  it(`should return the correct value if the`
+    + ` 'firstName' === undefined`, () => {
+    const usersForTesting = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(usersForTesting);
+
+    expect(usersForTesting).toEqual(usersMock);
+  });
 });
