a9e524e52835aa5f202b644c579294779ed27faa
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-11-03 14:36:22 +0200

    Tests

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,75 @@
 'use strict';
 
+const correctUsers = [
+  {
+    firstName: 'Elon',
+    lastName: 'Musk',
+    fullName: 'Elon Musk',
+  },
+  {
+    firstName: 'Tony',
+    lastName: 'Stark',
+    fullName: 'Tony Stark',
+  },
+  {
+    firstName: 'Bill',
+    lastName: 'Gates',
+    fullName: 'Bill Gates',
+  },
+];
+
+const { restoreNames } = require('./restoreNames');
+
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
-
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`should restore firstname if 'firstName'`
+    + `is empty, missing or undefined`, function() {
+    const brokenUsers = [
+      {
+        firstName: undefined,
+        lastName: 'Musk',
+        fullName: 'Elon Musk',
+      },
+      {
+        lastName: 'Stark',
+        fullName: 'Tony Stark',
+      },
+      {
+        firstName: '',
+        lastName: 'Gates',
+        fullName: 'Bill Gates',
+      },
+    ];
+
+    restoreNames(brokenUsers);
+
+    expect(brokenUsers).toEqual(correctUsers);
+  });
+
+  it(`shouldn't change users array if 'first name is exist'`, function() {
+    const brokenUsers = [
+      {
+        firstName: 'Musk',
+        lastName: 'Musk',
+        fullName: 'Elon Musk',
+      },
+      {
+        firstName: 'Tony',
+        lastName: 'Stark',
+        fullName: 'Tony Stark',
+      },
+      {
+        firstName: 'Bill',
+        lastName: 'Gates',
+        fullName: 'Bill Gates',
+      },
+    ];
+
+    restoreNames(brokenUsers);
+
+    expect(brokenUsers).toEqual(correctUsers);
+  });
 });
