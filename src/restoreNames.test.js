157842e5367494272a29037c684eed50d2c05a61
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2022-12-15 20:31:21 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,47 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it(`should be declared`, () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`should set name for existed firstName property`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName)
+      .toBe('Jack');
+  });
+
+  it(`should set new firstName property with name`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[1].firstName)
+      .toBe('Mike');
+  });
 });
