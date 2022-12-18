bee4f95d38fc95f2cd012ca3794a9d4e9ba9fe4f
Kyrylo Stadnyk <faust.ogr8@gmail.com>
2022-12-18 14:24:44 +0200

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,153 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  test('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  test('should restore the first name if first name is empty string', () => {
+    const user = [{
+      firstName: '',
+      lastName: 'Stadnyk',
+      fullName: 'Kyrylo Stadnyk',
+    }];
+
+    restoreNames(user);
+
+    expect(user)
+      .toStrictEqual([{
+        firstName: 'Kyrylo',
+        lastName: 'Stadnyk',
+        fullName: 'Kyrylo Stadnyk',
+      }]);
+  });
+
+  test('should restore the first name if first name is undefined', () => {
+    const user = [{
+      firstName: undefined,
+      lastName: 'Stadnyk',
+      fullName: 'Kyrylo Stadnyk',
+    }];
+
+    restoreNames(user);
+
+    expect(user)
+      .toStrictEqual([{
+        firstName: 'Kyrylo',
+        lastName: 'Stadnyk',
+        fullName: 'Kyrylo Stadnyk',
+      }]);
+  });
+
+  test('should restore the first name if first name is null', () => {
+    const user = [{
+      firstName: null,
+      lastName: 'Stadnyk',
+      fullName: 'Kyrylo Stadnyk',
+    }];
+
+    restoreNames(user);
+
+    expect(user)
+      .toStrictEqual([{
+        firstName: 'Kyrylo',
+        lastName: 'Stadnyk',
+        fullName: 'Kyrylo Stadnyk',
+      }]);
+  });
+
+  test('should restore the name if fullname is not empty', () => {
+    const user = [{
+      firstName: '',
+      lastName: 'Stadnyk',
+      fullName: 'Kyrylo Stadnyk',
+    }];
+
+    restoreNames(user);
+
+    expect(user)
+      .toStrictEqual([{
+        firstName: 'Kyrylo',
+        lastName: 'Stadnyk',
+        fullName: 'Kyrylo Stadnyk',
+      }]);
+  });
+
+  test('should restore the names of several objects', () => {
+    const users = [{
+      firstName: null,
+      lastName: 'Stadnyk',
+      fullName: 'Kyrylo Stadnyk',
+    },
+    {
+      firstName: undefined,
+      lastName: 'Ortega',
+      fullName: 'Jenna Ortega',
+    },
+    {
+      firstName: '',
+      lastName: 'Ricci',
+      fullName: 'Christina Ricci',
+    }];
+
+    restoreNames(users);
+
+    expect(users)
+      .toStrictEqual([{
+        firstName: 'Kyrylo',
+        lastName: 'Stadnyk',
+        fullName: 'Kyrylo Stadnyk',
+      },
+      {
+        firstName: 'Jenna',
+        lastName: 'Ortega',
+        fullName: 'Jenna Ortega',
+      },
+      {
+        firstName: 'Christina',
+        lastName: 'Ricci',
+        fullName: 'Christina Ricci',
+      }]);
+  });
+
+  test('should restore the names of several objects if at least one of them have the first name', () => {
+    const users = [{
+      firstName: 'Kyrylo',
+      lastName: 'Stadnyk',
+      fullName: 'Kyrylo Stadnyk',
+    },
+    {
+      firstName: undefined,
+      lastName: 'Ortega',
+      fullName: 'Jenna Ortega',
+    },
+    {
+      firstName: null,
+      lastName: 'Ricci',
+      fullName: 'Christina Ricci',
+    }];
+
+    restoreNames(users);
+
+    expect(users)
+      .toStrictEqual([{
+        firstName: 'Kyrylo',
+        lastName: 'Stadnyk',
+        fullName: 'Kyrylo Stadnyk',
+      },
+      {
+        firstName: 'Jenna',
+        lastName: 'Ortega',
+        fullName: 'Jenna Ortega',
+      },
+      {
+        firstName: 'Christina',
+        lastName: 'Ricci',
+        fullName: 'Christina Ricci',
+      }]);
+  });
 });
