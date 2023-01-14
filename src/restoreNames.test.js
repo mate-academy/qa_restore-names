af76c67fe0f40ccfc577af554d97744449e74362
Andrii Dydyk <andriidydyk622@gmail.com>
2023-01-14 14:40:40 +0100

    Fixed after review

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -3,7 +3,7 @@
 describe('restoreNames', () => {
   const { restoreNames } = require('./restoreNames');
 
-  it('should be declared', () => {
+  it(`should be declared`, () => {
     expect(restoreNames)
       .toBeInstanceOf(Function);
   });
@@ -46,6 +46,78 @@ describe('restoreNames', () => {
 
     expect(users[1].firstName)
       .toBe('Mike');
+  });
+
+  test(`should doesn't change firstName if firstName has a value`, () => {
+    const users = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack1 Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName)
+      .toBe('Jack');
+  });
+
+  test(`should show an empty array if users is empty`, () => {
+    const users = [];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual([]);
+  });
+
+  test(`should throw an error if users does not have fullName`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    expect(() => {
+      restoreNames(users);
+    }).toThrow();
+  });
+
+  test(`should restore firstName everywhere`
+  + ` if firstName missing or undefined  `, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'James Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName)
+      .toBe('Jack');
+
+    expect(users[1].firstName)
+      .toBe('Mike');
+
+    expect(users[2].firstName)
+      .toBe('James');
   });
   // write tests here
 });
