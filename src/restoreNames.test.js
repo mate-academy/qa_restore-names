8c8fb4a734b55f350dc510a264c026cdd7702cb6
Andrii Dydyk <andriidydyk622@gmail.com>
2023-01-14 10:34:38 +0100

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,51 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
+  test(`should restore firstName 'Jack' from fullName`
+  + `if it equal 'undefined'`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName)
+      .toBe('Jack');
+  });
+
+  test(`should restore firstName 'Mike' from fullName`
+  + `if it is missing`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[1].firstName)
+      .toBe('Mike');
+  });
   // write tests here
 });
