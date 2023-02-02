fc2604990ad3c4b8dedcb5309750ce9ce2a02312
Khrystyna Sabat <satttinka@gmail.com>
2023-02-02 23:02:30 +0100

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,67 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  const correctUser = [{
+    firstName: 'Mike',
+    lastName: 'Adams',
+    fullName: 'Mike Adams',
+  }];
 
+  it('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should add first name if it doesnot exist', () => {
+    const user = [{
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }];
+
+    restoreNames(user);
+
+    expect(user)
+      .toEqual(correctUser);
+  });
+
+  it('should add first name if it is empty', () => {
+    const user = [{
+      firstName: '',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }];
+
+    restoreNames(user);
+
+    expect(user)
+      .toEqual(correctUser);
+  });
+
+  it('should add first name if it is null', () => {
+    const user = [{
+      firstName: null,
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }];
+
+    restoreNames(user);
+
+    expect(user)
+      .toEqual(correctUser);
+  });
+
+  it('should add first name if it is undefined', () => {
+    const user = [{
+      firstName: undefined,
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }];
+
+    restoreNames(user);
+
+    expect(user)
+      .toEqual(correctUser);
+  });
 });
