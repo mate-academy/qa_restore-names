de6f5206b5cd3b9b3e2bbe63225ab59d01822870
Anatoliy Boiko <anatoliyb1414@gmail.com>
2023-02-15 12:13:21 +0200

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,26 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
+  const users = [
+    {
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+  ];
 
-  it('should ', () => {
+  restoreNames(users);
 
+  it('should restore first name of the first user ', () => {
+    expect(users[0].firstName).toBe('Jack');
   });
 
-  // write tests here
+  it('should create first name of the second user ', () => {
+    expect(users[1].firstName).toBe('Mike');
+  });
 });
