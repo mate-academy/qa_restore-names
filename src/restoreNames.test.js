1814bfa76d62936849743eb910d55acc9ed1c1bc
Anatoliy Boiko <anatoliyb1414@gmail.com>
2023-02-17 10:53:22 +0200

    New Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -12,6 +12,10 @@ describe('restoreNames', () => {
       lastName: 'Adams',
       fullName: 'Mike Adams',
     },
+    {
+      lastName: 'OfMe',
+      fullName: '',
+    },
   ];
 
   restoreNames(users);
@@ -20,7 +24,29 @@ describe('restoreNames', () => {
     expect(users[0].firstName).toBe('Jack');
   });
 
-  it('should create first name of the second user ', () => {
+  it('should create firstname atribute of the second user ', () => {
+    expect(users[1]).toHaveProperty('firstName');
+  });
+
+  it('should restore first name of the second user ', () => {
     expect(users[1].firstName).toBe('Mike');
+  });
+
+  const fullN = users[0].firstName + ' ' + users[0].lastName;
+
+  it('should have full name filled with firstname + lastname', () => {
+    expect(fullN).toEqual(users[0].fullName);
+  });
+
+  it('firstName will be created and empty when fullName is empty', () => {
+    if(users[2].lastName === '') {
+      expect(users[2].firstName).toEqual('');
+    }
+  });
+
+  it('firstName will be created and empty when fullName is empty', () => {
+    if(users[2].lastName === undefined) {
+      expect(users[2].firstName).toEqual('');
+    }
   });
 });
