a944717f91382ca3ebaf7a35af151fba395a3f84
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-02-24 00:02:45 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,68 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it(`firstName = 'undefined'`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users[0].firstName)
+      .toBe('Jack');
   });
 
-  // write tests here
+  it(`firstName is undefined`, () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName)
+      .toBe('Mike');
+  });
+
+  it(`deep equal`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual([
+        {
+          firstName: 'Jack',
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+        {
+          firstName: 'Mike',
+          lastName: 'Adams',
+          fullName: 'Mike Adams',
+        },
+      ]);
+  });
 });
