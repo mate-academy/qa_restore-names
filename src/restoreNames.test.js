db0abfaa83a108044d8db0ef2a69061ddf7f9c0c
Max Nikiforov <maxxnikiforov@gmail.com>
2023-03-26 22:31:25 +0700

    tests added

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,32 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should restore firstName', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName)
+      .toBe('Jack');
+
+    expect(users[1].firstName)
+      .toBe('Mike');
+  });
 });
