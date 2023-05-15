d265498dd739b6e96b8ee88952986da9d1c8cc47
Artem Sukhetskiy <artem.sukhetskiy@mirko.in.ua>
2023-05-15 10:28:25 +0300

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,70 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it(`'restoreNames' should be declarated`, () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`if 'firstName' is undefined`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Bodak',
+        fullName: 'Nazar Bodak',
+      },
+    ];
+
+    restoreNames(users);
+
+    const actual = users[0].firstName;
+    const expected = 'Nazar';
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`if there is no 'firstName'`, () => {
+    const users = [
+      {
+        lastName: 'Bodak',
+        fullName: 'Nazar Bodak',
+      },
+    ];
+
+    restoreNames(users);
+
+    const actual = users[0].firstName;
+    const expected = 'Nazar';
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`if there are 3 users with 'firstName' problem`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Bodak',
+        fullName: 'Nazar Bodak',
+      },
+      {
+        lastName: 'Oles',
+        fullName: 'Oleksandr Oles',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Poroshenko',
+        fullName: 'Petro Poroshenko',
+      },
+
+    ];
+
+    restoreNames(users);
+
+    const actual = users[0].firstName + ' '
+    + users[1].firstName + ' ' + users[2].firstName;
+    const expected = 'Nazar Oleksandr Petro';
+
+    expect(actual).toEqual(expected);
+  });
 });
