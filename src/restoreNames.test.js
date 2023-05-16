732fb47323512ac06447c47b8d4a7ab34602db6f
Oleh Chermak <ochermak2002@gmail.com>
2023-05-16 13:01:25 +0300

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,147 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should not return anything', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    expect(restoreNames(users))
+      .toBeUndefined();
+  });
+
+  it('should not modified user if it has firstName propertie', () => {
+    const users = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual(users);
+  });
+
+  it('should work if firstName equal to undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual([
+        {
+          firstName: 'Jack',
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+      ]);
+  });
+
+  it('should work if there is no firstName propertie in user object', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual([
+        {
+          firstName: 'Mike',
+          lastName: 'Adams',
+          fullName: 'Mike Adams',
+        },
+      ]);
+  });
+
+  it('should work with different scenarious', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+      {
+        firstName: 'Barry',
+        lastName: 'Allen',
+        fullName: 'Barry Allen',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Statham',
+        fullName: 'Jason Statham',
+      },
+      {
+        lastName: 'Mostavchuk',
+        fullName: 'Petro Mostavchuk',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual([
+        {
+          firstName: 'Jack',
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+        {
+          firstName: 'Mike',
+          lastName: 'Adams',
+          fullName: 'Mike Adams',
+        },
+        {
+          firstName: 'Barry',
+          lastName: 'Allen',
+          fullName: 'Barry Allen',
+        },
+        {
+          firstName: 'Jason',
+          lastName: 'Statham',
+          fullName: 'Jason Statham',
+        },
+        {
+          firstName: 'Petro',
+          lastName: 'Mostavchuk',
+          fullName: 'Petro Mostavchuk',
+        },
+      ]);
+  });
 });
