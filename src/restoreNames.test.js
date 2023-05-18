0465c357e820fa3e93456a3c4424278ef31514ba
Yurii Demkiv <demkiv123@gmail.com>
2023-05-18 09:19:17 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,74 @@
 'use strict';
 
+const { restoreNames } = require('./restoreNames');
+
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
-
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should not return anything', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    expect(restoreNames(users))
+      .toBeUndefined();
+  });
+
+  it('should set correct firstName if firstName is undefiend',
+    () => {
+      const users = [
+        {
+          firstName: undefined,
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+      ];
+
+      restoreNames(users);
+
+      expect(users)
+        .toEqual(
+          [
+            {
+              firstName: 'Jack',
+              lastName: 'Holy',
+              fullName: 'Jack Holy',
+            },
+          ]
+        );
+    });
+
+  it('should add firstName key with proper value if its not decalred',
+    () => {
+      const users = [
+        {
+          lastName: 'Adams',
+          fullName: 'Mike Adams',
+        },
+      ];
+
+      restoreNames(users);
+
+      expect(users)
+        .toEqual(
+          [
+            {
+              firstName: 'Mike',
+              lastName: 'Adams',
+              fullName: 'Mike Adams',
+            },
+          ]
+        );
+    });
 });
