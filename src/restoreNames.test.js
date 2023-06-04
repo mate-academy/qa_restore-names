d90a9e5f45a9956c9c48de07d6389440c0fbd566
Vlad <bojko.vladislav16@gmail.com>
2023-06-04 19:37:29 +0300

    All tests have been provided

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,44 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should replace undefined with real firstName', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];    
 
+    restoreNames(users);
+
+    expect(users).toStrictEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ])
   });
 
-  // write tests here
+  it('should add the firstName if it is missing', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];    
+
+    restoreNames(users);
+
+    expect(users).toStrictEqual([
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ])
+  });
 });
