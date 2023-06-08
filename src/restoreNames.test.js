f8f0ddc797df299cb4312ad2073fc24fb68b401b
Ihor Duhanets <igor_duganets@ukr.net>
2023-06-08 20:13:09 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,73 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should restore undefined firstNames', () => {
+    const users = [
+      {
+        firstName: undefined, lastName: 'Smith', fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Jane', lastName: 'Smith', fullName: 'Jane Smith',
+      },
+    ]);
+  });
+
+  it('should restore empty firstNames', () => {
+    const users = [
+      {
+        firstName: '', lastName: 'Lee', fullName: 'Anna Lee',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Anna', lastName: 'Lee', fullName: 'Anna Lee',
+      },
+    ]);
+  });
+
+  it('should restore missed firstNames', () => {
+    const users = [
+      {
+        lastName: 'Brown', fullName: 'Michael Brown',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Michael', lastName: 'Brown', fullName: 'Michael Brown',
+      },
+    ]);
+  });
+
+  it('should work with correct firstNames', () => {
+    const users = [
+      {
+        firstName: 'Mark', lastName: 'Johnson', fullName: 'Mark Johnson',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Mark', lastName: 'Johnson', fullName: 'Mark Johnson',
+      },
+    ]);
+  });
 });
