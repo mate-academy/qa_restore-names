240eddd5b5dcb955d3dc478e023fae36323b4447
VikkiPetrova <vikkipetrova55@gmail.com>
2023-06-08 18:18:28 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,48 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should restore firstName if it is undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Jack');
+    expect(users[1].firstName).toBe('Mike');
   });
 
-  // write tests here
+  it('should not modify firstName if it already has a value', () => {
+    const users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('John');
+    expect(users[1].firstName).toBe('Jane');
+  });
+
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
+  });
 });
