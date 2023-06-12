6111af371dea9c5abd30679feac868ea7d3ac0ba
Viktoriia Tykhomyrova <viktoriavintonak0@gmail.com>
2023-06-12 09:22:48 +0200

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,38 @@
+
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should set correct firstName for users without firstName', () => {
+    const users = [
+      {
+        firstName: '', lastName: 'Doe', fullName: 'John Doe',
+      },
+      {
+        firstName: undefined, lastName: 'Smith', fullName: 'Jane Smith',
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('John');
+    expect(users[1].firstName).toBe('Jane');
   });
 
-  // write tests here
+  it('should not modify firstName for users with existing firstName', () => {
+    const users = [
+      {
+        firstName: 'John', lastName: 'Doe', fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane', lastName: 'Smith', fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('John');
+    expect(users[1].firstName).toBe('Jane');
+  });
 });
