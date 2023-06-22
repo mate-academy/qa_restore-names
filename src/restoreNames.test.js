f692b5bbd5fa058aafb43eb5ef935ee347b44347
Ruslan Vrazovskyi <dnkons@gmail.com>
2023-06-22 21:50:40 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,65 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should restore first names based on full names', () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: '',
+        lastName: 'Smith',
+        fullName: 'Alice Smith',
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('John');
+    expect(users[1].firstName).toBe('Alice');
   });
 
-  // write tests here
+  it('should handle full names with uppercase and lowercase letters', () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Doe',
+        fullName: 'jOHN dOE',
+      },
+      {
+        firstName: '',
+        lastName: 'Smith',
+        fullName: 'aLICE sMITH',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('jOHN');
+    expect(users[1].firstName).toBe('aLICE');
+  });
+
+  it('should handle full names with special characters', () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Doe',
+        fullName: 'John@Doe',
+      },
+      {
+        firstName: '',
+        lastName: 'Smith',
+        fullName: 'Alice-Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('John@Doe');
+    expect(users[1].firstName).toBe('Alice-Smith');
+  });
 });
