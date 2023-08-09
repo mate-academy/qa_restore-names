f96f2f6eaa1f15ad493ca6e3a7751a62824b8323
Vadym Dubniak <dubnyakv@gmail.com>
2023-08-09 18:51:53 +0300

    implemented tests

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,78 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be a function', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should add firstname if its undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    const expectedResult = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual(expectedResult);
+  });
+
+  it('should add firstname if its empty string', () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    const expectedResult = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual(expectedResult);
+  });
+
+  it('should add firstname if its absent', () => {
+    const users = [
+      {
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    const expectedResult = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users)
+      .toEqual(expectedResult);
+  });
 });
