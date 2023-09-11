c4bd4f91512a65753f689ca25c3e995531628e84
Bohdan Svystun <bogdansvystun02@gmail.com>
2023-09-11 20:30:03 +0300

    second commit

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -3,64 +3,102 @@
 describe('restoreNames', () => {
   const { restoreNames } = require('./restoreNames');
 
-  describe('restoreNames', () => {
-    it('should restore firstName for users with undefined', () => {
-      const users = [
-        {
-          firstName: undefined,
-          lastName: 'Holy',
-          fullName: 'Jack Holy',
-        },
-        {
-          lastName: 'Adams',
-          fullName: 'Mike Adams',
-        },
-      ];
+  it('should restore firstName for users with undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
-      restoreNames(users);
+    restoreNames(users);
 
-      expect(users).toEqual([
-        {
-          firstName: 'Jack',
-          lastName: 'Holy',
-          fullName: 'Jack Holy',
-        },
-        {
-          firstName: 'Mike',
-          lastName: 'Adams',
-          fullName: 'Mike Adams',
-        },
-      ]);
-    });
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
+  });
 
-    it('should not modify firstName for users with existing firstName', () => {
-      const users = [
-        {
-          firstName: 'John',
-          lastName: 'Doe',
-          fullName: 'John Doe',
-        },
-        {
-          firstName: 'Jane',
-          lastName: 'Smith',
-          fullName: 'Jane Smith',
-        },
-      ];
+  it('should restore firstName for users with undefined', () => {
+    const users = [
+      {
+        firstName: null,
+        lastName: 'Holy',
+        fullName: 'John Holy',
+      },
+    ];
 
-      restoreNames(users);
+    restoreNames(users);
 
-      expect(users).toEqual([
-        {
-          firstName: 'John',
-          lastName: 'Doe',
-          fullName: 'John Doe',
-        },
-        {
-          firstName: 'Jane',
-          lastName: 'Smith',
-          fullName: 'Jane Smith',
-        },
-      ]);
-    });
+    expect(users).toEqual([
+      {
+        firstName: 'John',
+        lastName: 'Holy',
+        fullName: 'John Holy',
+      },
+    ]);
+  });
+
+  it('should restore firstName for users with undefined', () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Skort',
+        fullName: 'Bob Skort',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Bob',
+        lastName: 'Skort',
+        fullName: 'Bob Skort',
+      },
+    ]);
+  });
+
+  it('should not modify firstName for users with existing firstName', () => {
+    const users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ]);
   });
 });
