90547c9951ecc5ddd9a98dcef6459f720465f3ff
Olesia Semen <semenolesa01@gmail.com>
2023-10-15 00:38:03 +0300

    Testing

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,33 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
-  });
-
-  // write tests here
-});
+    it('should restore firstName for users with undefined or missing firstName', () => {
+      const users = [
+        {
+          firstName: undefined,
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+        {
+          lastName: 'Adams',
+          fullName: 'Mike Adams',
+        },
+      ];
+  
+      restoreNames(users);
+  
+      expect(users).toEqual([
+        {
+          firstName: 'Jack',
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+        {
+          firstName: 'Mike',
+          lastName: 'Adams',
+          fullName: 'Mike Adams',
+        },
+      ]);
+    })})
