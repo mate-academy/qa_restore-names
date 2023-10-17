c1d4ffebeb0419727667df3774e9d130c884d274
Artem Kravets <kravetsartyom@gmail.com>
2023-10-17 18:57:08 +0300

    Solution v2

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -2,33 +2,46 @@
 
 describe('restoreNames', () => {
   const { restoreNames } = require('./restoreNames');
+  const correctUsers = [{
+    firstName: 'Jack',
+    lastName: 'Holy',
+    fullName: 'Jack Holy',
+  },
+  {
+    firstName: 'Mike',
+    lastName: 'Adams',
+    fullName: 'Mike Adams',
+  },
+  ];
 
-  it('should create firstName, use name from fullName', () => {
-    const users = [
-      {
-        firstName: undefined,
-        lastName: 'Holy',
-        fullName: 'Jack Holy',
-      },
-      {
-        lastName: 'Adams',
-        fullName: 'Mike Adams',
-      },
-    ];
+  const users = [
+    {
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+  ];
 
+  it('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
+  });
+
+  it('should add correct firstName if it is undefined', () => {
     restoreNames(users);
 
-    expect(users).toEqual([
-      {
-        firstName: 'Jack',
-        lastName: 'Holy',
-        fullName: 'Jack Holy',
-      },
-      {
-        firstName: 'Mike',
-        lastName: 'Adams',
-        fullName: 'Mike Adams',
-      },
-    ]);
+    expect(users[0].firstName)
+      .toBe(correctUsers[0].firstName);
+  });
+
+  it('should add correct firstName if it does not exist', () => {
+    restoreNames(users);
+
+    expect(users[1].firstName)
+      .toBe(correctUsers[1].firstName);
   });
 });
