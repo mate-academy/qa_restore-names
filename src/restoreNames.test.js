b1c1d7156b07a0b60f8b9821f9c4399978ca303d
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-10-19 20:25:59 +0200

    restoreNames

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,48 +1,48 @@
-'use strict';
+"use strict";
 
-const { restoreNames } = require('./restoreNames');
+const { restoreNames } = require("./restoreNames");
 
-describe('restoreNames', () => {
-  it('should set correct firstName for users with undefined firstName', () => {
+describe("restoreNames", () => {
+  it("should set correct firstName for users with undefined firstName", () => {
     const users = [
       {
         firstName: undefined,
-        lastName: 'Holy',
-        fullName: 'Jack Holy',
+        lastName: "Holy",
+        fullName: "Jack Holy",
       },
       {
-        lastName: 'Adams',
-        fullName: 'Mike Adams',
+        lastName: "Adams",
+        fullName: "Mike Adams",
       },
     ];
 
     const modifiedUsers = restoreNames(users);
 
-    expect(modifiedUsers[0].firstName).toBe('Jack');
-    expect(modifiedUsers[1].firstName).toBe('Mike');
+    expect(modifiedUsers[0].firstName).toBe("Jack");
+    expect(modifiedUsers[1].firstName).toBe("Mike");
   });
 
-  it('should not modify firstName for users with defined firstName', () => {
+  it("should not modify firstName for users with defined firstName", () => {
     const users = [
       {
-        firstName: 'John',
-        lastName: 'Smith',
-        fullName: 'John Smith',
+        firstName: "John",
+        lastName: "Smith",
+        fullName: "John Smith",
       },
       {
-        firstName: 'Alice',
-        lastName: 'Johnson',
-        fullName: 'Alice Johnson',
+        firstName: "Alice",
+        lastName: "Johnson",
+        fullName: "Alice Johnson",
       },
     ];
 
     const modifiedUsers = restoreNames(users);
 
-    expect(modifiedUsers[0].firstName).toBe('John');
-    expect(modifiedUsers[1].firstName).toBe('Alice');
+    expect(modifiedUsers[0].firstName).toBe("John");
+    expect(modifiedUsers[1].firstName).toBe("Alice");
   });
 
-  it('should handle an empty array', () => {
+  it("should handle an empty array", () => {
     const users = [];
 
     const modifiedUsers = restoreNames(users);
@@ -51,106 +51,26 @@ describe('restoreNames', () => {
     expect(modifiedUsers).toEqual([]); // Additional check for an empty array
   });
 
-  it('should handle users with null firstName', () => {
+  it("should handle users with null firstName", () => {
     const users = [
       {
         firstName: null,
-        lastName: 'Smith',
-        fullName: 'John Smith',
+        lastName: "Smith",
+        fullName: "John Smith",
       },
       {
         firstName: null,
-        lastName: 'Johnson',
-        fullName: 'Alice Johnson',
+        lastName: "Johnson",
+        fullName: "Alice Johnson",
       },
     ];
 
     const modifiedUsers = restoreNames(users);
 
-    // Use a more robust check to handle any order of users
-    const userJohn = modifiedUsers.find(user => user.lastName === 'Smith');
-    const userAlice = modifiedUsers.find(user => user.lastName === 'Johnson');
+    const userJohn = modifiedUsers.find((user) => user.lastName === "Smith");
+    const userAlice = modifiedUsers.find((user) => user.lastName === "Johnson");
 
-    expect(userJohn.firstName).toBe('John');
-    expect(userAlice.firstName).toBe('Alice');
+    expect(userJohn.firstName).toBe("John");
+    expect(userAlice.firstName).toBe("Alice");
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-// describe('restoreNames', () => {
-//   it('should set correct firstName for users with undefined firstName', () => {
-//     const users = [
-//       {
-//         firstName: undefined,
-//         lastName: 'Holy',
-//         fullName: 'Jack Holy',
-//       },
-//       {
-//         lastName: 'Adams',
-//         fullName: 'Mike Adams',
-//       },
-//     ];
-
-//     restoreNames(users);
-
-//     expect(users[0].firstName).toBe('Jack');
-//     expect(users[1].firstName).toBe('Mike');
-//   });
-
-//   it('should not modify firstName for users with defined firstName', () => {
-//     const users = [
-//       {
-//         firstName: 'John',
-//         lastName: 'Smith',
-//         fullName: 'John Smith',
-//       },
-//       {
-//         firstName: 'Alice',
-//         lastName: 'Johnson',
-//         fullName: 'Alice Johnson',
-//       },
-//     ];
-
-//     restoreNames(users);
-
-//     expect(users[0].firstName).toBe('John');
-//     expect(users[1].firstName).toBe('Alice');
-//   });
-
-//   it('should handle an empty array', () => {
-//     const users = [];
-
-//     restoreNames(users);
-
-//     expect(users.length).toBe(0);
-//   });
-
-//   it('should handle users with null firstName', () => {
-//     const users = [
-//       {
-//         firstName: null,
-//         lastName: 'Smith',
-//         fullName: 'John Smith',
-//       },
-//       {
-//         firstName: null,
-//         lastName: 'Johnson',
-//         fullName: 'Alice Johnson',
-//       },
-//     ];
-
-//     restoreNames(users);
-
-//     expect(users[0].firstName).toBe('John');
-//     expect(users[1].firstName).toBe('Alice');
-//   });
-// });
