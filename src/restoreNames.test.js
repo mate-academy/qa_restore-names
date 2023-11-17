d7b25eb5c25781f3f7596b12f6a70c798d82fe7c
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-17 07:33:31 +0200

    restoreNames.test.js

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,41 @@
 'use strict';
 
+const { restoreNames } = require('./restoreNames');
+
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
-
-  it('should ', () => {
-
+  it('should restore firstName when it is undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+    restoreNames(users);
+    expect(users[0].firstName).toBe('Jack');
   });
 
-  // write tests here
+  it('should restore firstName when it is not present', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+    restoreNames(users);
+    expect(users[0].firstName).toBe('Mike');
+  });
+
+  it('should not change firstName when it is present and correct', () => {
+    const users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+    ];
+    restoreNames(users);
+    expect(users[0].firstName).toBe('John');
+  });
 });
+
