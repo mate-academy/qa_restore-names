2b8fd6cfc969bd83d76b665ecad91015b7c5fae2
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-12-15 03:12:19 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,48 @@
 'use strict';
 
+const { restoreNames } = require('./restoreNames');
+
+const users = [
+  {
+    firstName: undefined,
+    lastName: 'Holy',
+    fullName: 'Jack Holy',
+  },
+  {
+    lastName: 'Adams',
+    fullName: 'Mike Adams',
+  },
+];
+
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  it("adds firstName to each user if it is undefined or doesn't exist", () => {
+    restoreNames(users);
 
-  it('should ', () => {
-
+    expect(users[0].firstName).toBe('Jack');
+    expect(users[1].firstName).toBe('Mike');
   });
 
-  // write tests here
+  it("doesn't change existing names of the users", () => {
+    const usersWithNames = [
+      {
+        firstName: 'Michael',
+        lastName: 'Jackson',
+        fullName: 'Michael Jackson',
+      },
+      {
+        firstName: 'Joe',
+        lastName: 'Biden',
+        fullName: 'Joe Biden',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(usersWithNames[0].firstName).toBe('Michael');
+    expect(usersWithNames[1].firstName).toBe('Joe');
+  });
+
+  it("doesn't return anything", () => {
+    expect(restoreNames(users)).toBe(undefined);
+  });
 });
