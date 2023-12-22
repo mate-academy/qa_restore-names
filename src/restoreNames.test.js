0038314831ece82c6640a7b91e2c392018c31ce6
Kate <ekaterina2002aleks@gmail.com>
2023-12-22 17:15:35 +0200

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,62 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should set correct firstName to users', () => {
+    const expected = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    expect(restoreNames(users)).toEqual(expected);
   });
 
-  // write tests here
+  it('should left the same firstName if it is', () => {
+    const expected = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    const users = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    expect(restoreNames(users)).toEqual(expected);
+  });
+
+  it('should return empty array for empty array', () => {
+    const expected = [];
+
+    const users = [];
+
+    expect(restoreNames(users)).toEqual(expected);
+  });
 });
