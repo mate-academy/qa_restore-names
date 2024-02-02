a7d8196877cf0f44e2db98bc503a4621c1c56e91
Anastasiya145 <a.ivanova-external@powernext.com>
2024-02-02 12:26:51 +0100

    the tests were written

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,79 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should restore the right users firstnames when they are underfined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Ivanova',
+        fullName: 'Anastasiya Ivanova',
+      }
+    ];
+
+    restoreNames(users);
+
+    const usersFirstNames = users.map(user => user.firstName);
+
+    const expectedResult = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Anastasiya',
+        lastName: 'Ivanova',
+        fullName: 'Anastasiya Ivanova',
+      },
+    ];
+
+    const expectedUsersFirstNames = expectedResult.map(user => user.firstName);
+
+    expect(usersFirstNames).toEqual(expectedUsersFirstNames);
+  });
+
+  it('should restore the right users firstnames when they are not provided', () => {
+    const users = [
+      {
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Ivanova',
+        fullName: 'Anastasiya Ivanova',
+      }
+    ];
+
+    restoreNames(users);
+
+    const usersFirstNames = users.map(user => user.firstName);
+
+    const expectedResult = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Anastasiya',
+        lastName: 'Ivanova',
+        fullName: 'Anastasiya Ivanova',
+      },
+    ];
+
+    const expectedUsersFirstNames = expectedResult.map(user => user.firstName);
+
+    expect(usersFirstNames).toEqual(expectedUsersFirstNames);
+  });
 });
