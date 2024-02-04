8e5297eda31781ac42ae1f8e0901c9c516764ae8
Julia14012000 <Juliamilan1414@gmail.com>
2024-02-04 21:21:45 +0200

    solution restoreNames

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,39 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  const users = [
+    {
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+    {
+      lastName: 'Milian',
+      fullName: 'Yulia Milian',
+    },
 
+    {
+      firstName: 'Olia',
+      lastName: 'Holy',
+      fullName: 'Olia Holy',
+    },
+
+  ];
+
+  it('should return first name', () => {
+    restoreNames(users);
+    expect(users[0].firstName).toEqual('Jack');
+    expect(users[1].firstName).toEqual('Mike');
+    expect(users[2].firstName).toEqual('Yulia');
+    expect(users[3].firstName).toEqual('Olia');
+
+    expect(() => restoreNames(users))
+      .not.toThrow();
   });
-
-  // write tests here
 });
