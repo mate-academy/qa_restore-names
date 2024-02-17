06132f8868421132882d1d5a6c20ecea704a9c43
oleksandrshtonda <oleksandrshtonda@gmail.com>
2024-02-17 21:19:10 +0100

    implemented tests for restoreNames function

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,58 @@
 'use strict';
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+describe('restoreNames should restore "firstName field"', () => {
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('user.firstName has undefined value', () => {
+    const userToRestoring = [{
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
 
+    const userAfterRestoring = [{
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    restoreNames(userToRestoring);
+
+    expect(userToRestoring).toEqual(userAfterRestoring);
   });
 
-  // write tests here
+  it('user.firstName.length === 0', () => {
+    const userToRestoring = [{
+      lastName: 'Uzumaki',
+      fullName: 'Naruto Uzumaki',
+    }];
+
+    const userAfterRestoring = [{
+      firstName: 'Naruto',
+      lastName: 'Uzumaki',
+      fullName: 'Naruto Uzumaki',
+    }];
+
+    restoreNames(userToRestoring);
+
+    expect(userToRestoring).toEqual(userAfterRestoring);
+  });
+
+  it('user doesn\'t have the firstName property', () => {
+    const userToRestoring = [{
+      firstName: '',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }];
+
+    const userAfterRestoring = [{
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }];
+
+    restoreNames(userToRestoring);
+
+    expect(userToRestoring).toEqual(userAfterRestoring);
+  });
 });
