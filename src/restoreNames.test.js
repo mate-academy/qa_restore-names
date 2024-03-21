6db6b10ba6d7dd44d3ca3f97cd4d16c5704a8c72
Katerina Poliit <alexispoliyit@gmail.com>
2024-03-21 16:22:34 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,104 @@
 'use strict';
 
+const { restoreNames } = require('./restoreNames');
+
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  it('should set correct firstName to users with undefined firstName', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
-  it('should ', () => {
+    restoreNames(users);
 
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
   });
 
-  // write tests here
+  it('should set correct firstName to users with firstName equal to undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ]);
+  });
+
+  it('should not modify users with defined firstName', () => {
+    const users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ]);
+  });
 });
