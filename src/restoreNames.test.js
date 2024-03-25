770793ce44203cdd2b8da83915b591c35db2a296
Katerina Poliit <alexispoliyit@gmail.com>
2024-03-25 11:39:34 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -3,15 +3,15 @@
 const { restoreNames } = require('./restoreNames');
 
 describe('restoreNames', () => {
-  it('should set firstName if it is undefined', () => {
+  it(' should add a firstName if it is missing', () => {
     const users = [
       {
-        firstName: undefined,
+
         lastName: 'Holy',
         fullName: 'Jack Holy',
       },
       {
-        firstName: 'John',
+
         lastName: 'Doe',
         fullName: 'John Doe',
       },
@@ -38,6 +38,36 @@ describe('restoreNames', () => {
         firstName: 'Mike',
         lastName: 'Adams',
         fullName: 'Mike Adams',
+      },
+    ]);
+  });
+
+  it('should set firstName if it is undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
       },
     ]);
   });
@@ -72,3 +102,40 @@ describe('restoreNames', () => {
     ]);
   });
 });
+
+it('should add first name if it is empty string', () => {
+  const users = [
+    {
+      firstName: '',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      firstName: '',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+  ];
+
+  restoreNames(users);
+
+  expect(users).toEqual([
+    {
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+  ]);
+});
+
+it('should processing empty user array', () => {
+  const users = [];
+  restoreNames(users);
+  expect(users).toEqual([]);
+});
+
