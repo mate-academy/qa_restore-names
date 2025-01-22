23fa67f1279af321acd6ad0931ff968e235c38fd
Vladyslav Shylin <indiggoo228@gmail.com>
2025-01-22 17:27:40 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,79 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be a function', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should not return anything', () => {
+    expect(restoreNames([])).toBeUndefined();
+  });
+
+  it('should set right firstname for all users in array', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    const result = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+    expect(users).toEqual(result);
+  });
+
+  it('should set right firstname if firstname is undefined', () => {
+    const users = [{
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    const result = [{
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
+
+    restoreNames(users);
+    expect(users).toEqual(result);
+  });
+
+  it('should set right firstname if user dont have firstname', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+    const result = [
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+    expect(users).toEqual(result);
+  });
 });
