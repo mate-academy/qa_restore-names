65e6eaeec73f35dedee13897d8c714ae479fed12
ab3MN <ab3mn3@gmail.com>
2025-02-19 17:18:24 +0200

    feat:add solution

--- src/restoreNames.js
+++ src/restoreNames.js
@@ -9,11 +9,39 @@
  * @param {User[]} users
  */
 function restoreNames(users) {
+  if (!Array.isArray(users)) {
+    throw new Error('Input must be array of objects');
+  }
+
   for (const user of users) {
-    if (!user.firstName) {
-      [user.firstName] = user.fullName.split(' ');
+    if (Object.prototype.toString.call(user) !== '[object Object]') {
+      throw new Error('Input must be array of objects');
+    }
+
+    if (!user.fullName) {
+      throw new Error('FullName is required');
+    }
+
+    if (!user.firstName || !user.lastName) {
+      const fullName = user.fullName.split(' ');
+
+      if (!user.firstName) {
+        user.firstName = fullName[0];
+      }
+
+      if (!user.lastName) {
+        if (fullName.length === 1) {
+          user.lastName = '';
+        } else if (fullName.length > 2) {
+          user.lastName = fullName.slice(1).join(' ');
+        } else {
+          user.lastName = fullName.at(-1);
+        }
+      }
     }
   }
+
+  return users;
 }
 
 module.exports = { restoreNames };
