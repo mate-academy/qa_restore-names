7577323066ca9da040835b18918bc80d06c8917b
DDefty <dawid.kogut99@gmail.com>
2025-02-23 15:51:47 +0100

    test: add test for restoreNames

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,73 @@
 'use strict';
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+describe('restoreNames function test', () => {
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  test('Should restore first name from full name', () => {
+    const users = [
+      {
+        firstName: '', lastName: 'Doe', fullName: 'John Doe',
+      },
+      {
+        firstName: '', lastName: 'Smith', fullName: 'Jane Smith',
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John', lastName: 'Doe', fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane', lastName: 'Smith', fullName: 'Jane Smith',
+      },
+    ]);
   });
 
-  // write tests here
+  test('Shold not change first if they are already set', () => {
+    const users = [
+      {
+        firstName: 'John', lastName: 'Doe', fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane', lastName: 'Smith', fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John', lastName: 'Doe', fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane', lastName: 'Smith', fullName: 'Jane Smith',
+      },
+    ]);
+  });
+
+  test('Should handle empty array', () => {
+    const users = [];
+
+    restoreNames(users);
+
+    expect(users).toEqual([]);
+  });
+
+  test('Should handle empty full name', () => {
+    const users = [
+      {
+        firstName: '', lastName: '', fullName: '',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: '', lastName: '', fullName: '',
+      },
+    ]);
+  });
 });
