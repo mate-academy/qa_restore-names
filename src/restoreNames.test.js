3e00b7441eebbc97eb23818c2f71ca5476a73723
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2025-02-25 10:37:50 +0100

    feat: add tests

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,79 @@
-'use strict';
+"use strict";
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+describe("restoreNames", () => {
+  const { restoreNames } = require("./restoreNames");
 
-  it('should ', () => {
-
+  it("should be declared", () => {
+    expect(restoreNames).toBeDefined();
   });
 
-  // write tests here
+  it("should restore name if undefined was given", () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: "Greenfield",
+        fullName: "John Greenfield",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: "John",
+        lastName: "Greenfield",
+        fullName: "John Greenfield",
+      },
+    ]);
+  });
+
+  it("should restore name if it was empty string", () => {
+    const users = [
+      {
+        firstName: "",
+        lastName: "Malbrock",
+        fullName: "Ivan Malbrock",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: "Ivan",
+        lastName: "Malbrock",
+        fullName: "Ivan Malbrock",
+      },
+    ]);
+  });
+
+  it("should fill names when some objects are given", () => {
+    const users = [
+      {
+        firstName: "",
+        lastName: "Malbrock",
+        fullName: "Ivan Malbrock",
+      },
+      {
+        firstName: "",
+        lastName: "Muller",
+        fullName: "Kristian Muller",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: "Ivan",
+        lastName: "Malbrock",
+        fullName: "Ivan Malbrock",
+      },
+      {
+        firstName: "Kristian",
+        lastName: "Muller",
+        fullName: "Kristian Muller",
+      },
+    ]);
+  });
 });
