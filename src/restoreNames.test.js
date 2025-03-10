6ee95102370064dfb19e966ac3c5b1c3c48d583b
Maria <m.gosp1704@gmail.com>
2025-03-10 16:15:12 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,78 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should mutate an array and do not create a new one', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+    const expected = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
+    restoreNames(users);
+    expect(users).toEqual(expected);
   });
 
-  // write tests here
+  it('should return an emty array if users = []', () => {
+    const users = [];
+
+    restoreNames(users);
+    expect(users).toEqual([]);
+  });
+
+  it(`should not modify 'firstName' if it was already in array`, () => {
+    const users = [
+      {
+        firstName: 'Alice', lastName: 'Brown', fullName: 'Alice Brown',
+      },
+      {
+        firstName: 'Bob', lastName: 'Taylor', fullName: 'Bob Taylor',
+      },
+    ];
+
+    const originalCopy = JSON.parse(JSON.stringify(users));
+
+    restoreNames(users);
+
+    expect(users).toEqual(originalCopy);
+  });
+
+  it(`should use 'fullName' if 'fullName' consists only of 1 word`, () => {
+    const users = [
+      {
+        lastName: '',
+        fullName: 'Jack',
+      },
+    ];
+    const expected = [
+      {
+        firstName: 'Jack',
+        lastName: '',
+        fullName: 'Jack',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual(expected);
+  });
 });
