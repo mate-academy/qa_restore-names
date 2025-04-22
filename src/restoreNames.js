c531d2b0bb741db0cddd5d0924ca86eeb81a3192
Daria <dahaorlova953@gmail.com>
2025-04-22 18:10:12 +0300

    Solution

--- src/restoreNames.js
+++ src/restoreNames.js
@@ -2,15 +2,15 @@
 
 /**
  * @typedef {Object} User
- * @property {string} firstName
+ * @property {string} [firstName]
  * @property {string} lastName
- * @property {string} fullName
+ * @property {string} [fullName]
  *
  * @param {User[]} users
  */
 function restoreNames(users) {
   for (const user of users) {
-    if (!user.firstName) {
+    if (!user.firstName && typeof user.fullName === 'string') {
       [user.firstName] = user.fullName.split(' ');
     }
   }
--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,93 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(
+    'should restore firstName from fullName if firstName is undefined',
+    () => {
+      const users = [
+        {
+          firstName: undefined,
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+      ];
+
+      restoreNames(users);
+
+      expect(users[0].firstName).toBe('Jack');
+    }
+  );
+
+  it('should restore firstName for multiple users if missing', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Smith',
+        fullName: 'Anna Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Mike');
+    expect(users[1].firstName).toBe('Anna');
+  });
+
+  it('should not change firstName if it is already set', () => {
+    const users = [
+      {
+        firstName: 'Lily',
+        lastName: 'Brown',
+        fullName: 'Lily Brown',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Lily');
+  });
+
+  it('should do nothing if the array is empty', () => {
+    const users = [];
+
+    restoreNames(users);
+
+    expect(users).toEqual([]);
+  });
+
+  it(
+    'should handle users with missing fullName gracefully',
+    () => {
+      const users = [
+        {
+          lastName: 'Doe',
+          firstName: undefined,
+        },
+        {
+          fullName: null,
+          firstName: undefined,
+        },
+        {
+          fullName: '',
+          firstName: undefined,
+        },
+      ];
+
+      restoreNames(users);
+
+      expect(users[0].firstName).toBeUndefined();
+      expect(users[1].firstName).toBeUndefined();
+      expect(users[2].firstName).toBe('');
+    }
+  );
 });
