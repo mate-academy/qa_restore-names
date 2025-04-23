2b099cf2dbf1abc326ead86c3a21551870fc522c
Janani <jananiraja1405@gmail.com>
2025-04-23 16:06:43 +1200

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,10 +1,30 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should restore first Name from full name ', () => {
+    const users = [{
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      firstName: '',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+    {
+      lastName: 'Mouse',
+      fullName: 'Mickey Mouse',
+    },
+    ];
 
+    restoreNames(users);
+
+    users.forEach(user => {
+      expect(user.firstName).toBe(user.fullName.split(' ')[0]);
+    });
   });
 
   // write tests here
