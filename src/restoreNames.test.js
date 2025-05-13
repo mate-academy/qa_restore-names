455610730b6497e03bb12a7d289d85081d4d3cec
Maryna <marina.avramenko123@gmail.com>
2025-05-13 19:06:17 +0300

    fix problem

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,75 +1,73 @@
 "use strict";
 
 describe("restoreNames", () => {
-  describe("restoreNames", () => {
-    it("should ", () => {
-      it("should ", () => {});
-    });
-
-    const users = [
-      {
-        firstName: undefined,
-        lastName: "Smith",
-        fullName: "John Smith",
-      },
-      {
-        firstName: undefined,
-        lastName: "Doe",
-        fullName: "Jane Doe",
-      },
-    ];
-
-    restoreNames(users);
-
-    expect(users).toEqual([
-      {
-        firstName: "John",
-        lastName: "Smith",
-        fullName: "John Smith",
-      },
-      {
-        firstName: "Jane",
-        lastName: "Doe",
-        fullName: "Jane Doe",
-      },
-    ]);
+  it("should ", () => {
+    it("should ", () => {});
   });
 
-  it("should not modify users with existing firstName", () => {
-    const users = [
-      {
-        firstName: "Alice",
-        lastName: "Brown",
-        fullName: "Alice Brown",
-      },
-      {
-        firstName: "Bob",
-        lastName: "White",
-        fullName: "Bob White",
-      },
-    ];
+  const users = [
+    {
+      firstName: undefined,
+      lastName: "Smith",
+      fullName: "John Smith",
+    },
+    {
+      firstName: undefined,
+      lastName: "Doe",
+      fullName: "Jane Doe",
+    },
+  ];
 
-    restoreNames(users);
+  restoreNames(users);
 
-    expect(users).toEqual([
-      {
-        firstName: "Alice",
-        lastName: "Brown",
-        fullName: "Alice Brown",
-      },
-      {
-        firstName: "Bob",
-        lastName: "White",
-        fullName: "Bob White",
-      },
-    ]);
-  });
+  expect(users).toEqual([
+    {
+      firstName: "John",
+      lastName: "Smith",
+      fullName: "John Smith",
+    },
+    {
+      firstName: "Jane",
+      lastName: "Doe",
+      fullName: "Jane Doe",
+    },
+  ]);
+});
 
-  it("should handle an empty array without errors", () => {
-    const users = [];
+it("should not modify users with existing firstName", () => {
+  const users = [
+    {
+      firstName: "Alice",
+      lastName: "Brown",
+      fullName: "Alice Brown",
+    },
+    {
+      firstName: "Bob",
+      lastName: "White",
+      fullName: "Bob White",
+    },
+  ];
 
-    restoreNames(users);
+  restoreNames(users);
 
-    expect(users).toEqual([]);
-  });
+  expect(users).toEqual([
+    {
+      firstName: "Alice",
+      lastName: "Brown",
+      fullName: "Alice Brown",
+    },
+    {
+      firstName: "Bob",
+      lastName: "White",
+      fullName: "Bob White",
+    },
+  ]);
+});
+
+it("should handle an empty array without errors", () => {
+  const users = [];
+
+  restoreNames(users);
+
+  expect(users).toEqual([]);
 });
