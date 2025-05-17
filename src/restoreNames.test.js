3878d42e860882480544598bedc6e8cf72eed0e6
Yurii_Popovskiy <ronondex663@gmail.com>
2025-05-17 10:49:50 +0300

    restoreNameTestAdd

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,42 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
   it('should ', () => {
 
   });
 
-  // write tests here
+  it('should restore undefined firstName from fullName', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Wade',
+        fullName: 'John Holl',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('John');
+  });
+
+  it('should restore firstName for multiple users', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holl',
+        fullName: 'John Holl',
+      },
+      {
+        lastName: 'Mikola',
+        fullName: 'Leo Mikola',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('John');
+    expect(users[1].firstName).toBe('Leo');
+  });
 });
