928c59cf332444992f461ad7c9fd01d893d5f20d
Inna <innadynka@gmail.com>
2025-06-09 16:29:33 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,30 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const restoreNames = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('Object in function', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Object);
   });
 
-  // write tests here
+  it('should not change object', () => {
+    const users = [{ firstName: 'Sarah' }];
+
+    expect(users[0].firstName).toBe('Sarah');
+  });
+
+  it('should not change first name', () => {
+    const users = [{
+      firstName: 'Mike', fullName: 'Michael Johnson',
+    }];
+
+    expect(users[0].firstName).toBe('Mike');
+  });
+
+  it('should work if first name was missing', () => {
+    const users = [{ }];
+
+    expect(() => restoreNames(users)).toThrow();
+  });
 });
