6fdbc47ff6fad6e75a92eb7f544033578cf1bae4
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-06-16 11:34:53 +0100

    QA restore names solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,71 @@
-'use strict';
+"use strict";
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+describe("restoreNames", () => {
+  const { restoreNames } = require("./restoreNames");
 
-  it('should ', () => {
+  it("should restore name, if it is lost", () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
 
+      {
+        firstName: "Joe",
+        lastName: "Doe",
+        fullName: "Joe Doe",
+      },
+    ];
+
+    const expectedUsers = [
+      {
+        firstName: "Jack",
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+
+      {
+        firstName: "Joe",
+        lastName: "Doe",
+        fullName: "Joe Doe",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual(expectedUsers);
   });
 
-  // write tests here
+  it("should not modify users with existing firstName", () => {
+    const users = [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        fullName: "John Doe",
+      },
+    ];
+
+    const expected = [...users];
+
+    restoreNames(users);
+    expect(users).toEqual(expected);
+  });
+
+  it("should handle empty array", () => {
+    const users = [];
+    const expected = [];
+
+    restoreNames(users);
+    expect(users).toEqual(expected);
+  });
 });
