4ccf5542cabc459576baf7e1e51635406cde9e4d
Yuliia <shevchuuchkaaa@gmail.com>
2025-06-17 16:25:06 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,52 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should add name from fullName if user do not have it', () => {
+    const users = [
+      {
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
 
+    const user = users[0];
+
+    restoreNames(users);
+
+    expect(user.firstName).toBe('Jack');
   });
 
-  // write tests here
+  it('should add name from fullName if userName is undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    const user = users[0];
+
+    restoreNames(users);
+
+    expect(user.firstName).toBe('Jack');
+  });
+
+  it('should add name from fullName if userName is empty string', () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    const user = users[0];
+
+    restoreNames(users);
+
+    expect(user.firstName).toBe('Jack');
+  });
 });
