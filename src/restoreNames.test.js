8689122ee9610226fff4cb50cc3c9b2b5527fd5c
Heny <jackbalageru@yahoo.com>
2025-07-01 10:31:43 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,57 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should work with an empty array', () => {
+    expect(restoreNames([])).toBeUndefined();
+  });
+
+  it(`should restore if 'firstName' is lost`, () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+    expect(users[0].firstName).toBe('Mike');
+  });
+
+  it(`should restore if 'firstName' equals to undefined`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+    expect(users[0].firstName).toBe('Jack');
+  });
+
+  it('should change multiple users', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Jack');
+    expect(users[1].firstName).toBe('Mike');
+  });
 });
