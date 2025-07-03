96ff78645392a672bedf90a48cde3bb2d837b40f
Alina Kuliak <kuljakalina@gmail.com>
2025-07-03 13:03:29 +0200

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,78 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should restore firstName from fullName when firstName is undefined',
+    () => {
+      const users = [
+        {
+          firstName: undefined, lastName: 'Holy', fullName: 'Jack Holy',
+        },
+        {
+          firstName: undefined, lastName: 'Adams', fullName: 'Mike Adams',
+        },
+      ];
 
+      restoreNames(users);
+
+      expect(users).toEqual([
+        {
+          firstName: 'Jack', lastName: 'Holy', fullName: 'Jack Holy',
+        },
+        {
+          firstName: 'Mike', lastName: 'Adams', fullName: 'Mike Adams',
+        },
+      ]);
+    });
+
+  it('should restore firstName from fullName when firstName is null', () => {
+    const users = [
+      {
+        firstName: null, lastName: 'Brown', fullName: 'Sara Brown',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Sara', lastName: 'Brown', fullName: 'Sara Brown',
+      },
+    ]);
   });
 
-  // write tests here
+  it('should restore firstName from fullName when firstName is an empty string',
+    () => {
+      const users = [
+        {
+          firstName: '', lastName: 'Stone', fullName: 'Lily Stone',
+        },
+      ];
+
+      restoreNames(users);
+
+      expect(users).toEqual([
+        {
+          firstName: 'Lily', lastName: 'Stone', fullName: 'Lily Stone',
+        },
+      ]);
+    });
+
+  it('should not change firstName if it already exists', () => {
+    const users = [
+      {
+        firstName: 'Anna', lastName: 'Lee', fullName: 'Anna Lee',
+      },
+      {
+        firstName: 'John', lastName: 'Smith', fullName: 'John Smith',
+      },
+    ];
+
+    const original = JSON.parse(JSON.stringify(users));
+
+    restoreNames(users);
+
+    expect(users).toEqual(original);
+  });
 });
