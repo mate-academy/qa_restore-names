6a9a27a106f3e6542a4db7ca30d3a03693b98c6a
Marianna Matola <mutnenka.mk@gmail/com>
2025-07-05 23:49:09 +0300

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,79 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should work with empty firstName', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toEqual('Mike');
+  });
+
+  it('should work with undefined firstName', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toEqual('Jack');
+  });
+
+  it('should work with empty string firstName', () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toEqual('Jack');
+  });
+
+  it('should work with null firstName', () => {
+    const users = [
+      {
+        firstName: null,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toEqual('Jack');
+  });
+
+  it('should not make changes with valid firstName', () => {
+    const users = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toEqual('Jack');
+  });
+
 });
