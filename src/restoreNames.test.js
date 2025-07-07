057a37a363d06ec7fbd3156342abd711e957ef62
Iryna Zahorodnia <irishas25@ukr.net>
2025-07-07 15:11:44 +0300

    test restore names

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,83 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should restore firstName if it is undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Jack');
+  });
+
+  it('should restore firstName if it does not exist', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Mike');
+  });
+
+  it('should restore firstName for several users', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    const restoredUsers = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual(restoredUsers);
+  });
+
+  it('should not change user if it has firstName', () => {
+    const users = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Jack');
+    expect(users[0].lastName).toBe('Holy');
+    expect(users[0].fullName).toBe('Jack Holy');
+  });
 });
