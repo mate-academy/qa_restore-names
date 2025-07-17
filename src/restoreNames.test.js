c0987f8390ab0ae8aa1c78488aa29db7927e1607
syavaYki <s_podolskiy@live.com>
2025-07-17 18:01:04 -0400

    fixed blank test

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -3,8 +3,6 @@
 describe('restoreNames', () => {
   const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {});
-
   it('should be a function', () => {
     expect(typeof restoreNames).toBe('function');
   });
@@ -29,6 +27,7 @@ describe('restoreNames', () => {
         fullName: 'Mike Adams',
       },
     ];
+
     restoreNames(users);
     expect(users[0].firstName).toBe('Mike');
   });
@@ -41,37 +40,43 @@ describe('restoreNames', () => {
         fullName: 'John Doe',
       },
     ];
+
     restoreNames(users);
     expect(users[0].firstName).toBe('John');
   });
 
   it('should handle an empty array of users', () => {
     const users = [];
+
     restoreNames(users);
     expect(users).toEqual([]);
   });
 
-  it('should handle multiple users, some with missing first names and some with existing', () => {
-    const users = [
-      {
-        firstName: undefined,
-        lastName: 'Holy',
-        fullName: 'Jack Holy',
-      },
-      {
-        firstName: 'Jane',
-        lastName: 'Doe',
-        fullName: 'Jane Doe',
-      },
-      {
-        firstName: undefined,
-        lastName: 'Adams',
-        fullName: 'Mike Adams',
-      },
-    ];
-    restoreNames(users);
-    expect(users[0].firstName).toBe('Jack');
-    expect(users[1].firstName).toBe('Jane');
-    expect(users[2].firstName).toBe('Mike');
-  });
+  it(
+    'should handle, some with missing' + 'first names and some with existing',
+    () => {
+      const users = [
+        {
+          firstName: undefined,
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+        {
+          firstName: 'Jane',
+          lastName: 'Doe',
+          fullName: 'Jane Doe',
+        },
+        {
+          firstName: undefined,
+          lastName: 'Adams',
+          fullName: 'Mike Adams',
+        },
+      ];
+
+      restoreNames(users);
+      expect(users[0].firstName).toBe('Jack');
+      expect(users[1].firstName).toBe('Jane');
+      expect(users[2].firstName).toBe('Mike');
+    }
+  );
 });
