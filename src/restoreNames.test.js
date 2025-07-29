6a68ed0012ba527ca9b801fb786c353abb861a6e
Renat <renatabdrah001@gmail.com>
2025-07-29 17:09:31 +0200

    add task solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,40 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`should restore user${'`s'} first name`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    const userFirstName = users[0].firstName;
+
+    expect(userFirstName).toBe('Jack');
+  });
+
+  it(`should restore 'firstName' and user${'`s'} first name`, () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    const userFirstName = users[0].firstName;
+
+    expect(userFirstName).toBe('Mike');
+  });
 });
