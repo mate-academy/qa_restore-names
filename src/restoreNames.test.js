273adc77e15aa547414ab00c3bca837c5e7d4054
damian Buń <damian.bun00@gmail.com>
2025-08-17 10:03:05 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,90 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const {
+    restoreNames,
+  } = require('./restoreNames.js');
 
-  it('should ', () => {
+  it('should return firstName when is undefined', () => {
+    const users = [{
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }];
 
+    restoreNames(users);
+
+    expect(users).toEqual([{
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }]);
   });
 
-  // write tests here
+  it('should return firstName when is missing', () => {
+    const users = [{
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }];
+
+    restoreNames(users);
+
+    expect(users).toEqual([{
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }]);
+  });
+
+  it('should not modify array when firstName is already present', () => {
+    const users = [{
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }];
+
+    restoreNames(users);
+
+    expect(users).toEqual([{
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }]);
+  });
+
+  it('sholud handle empty array', () => {
+    const users = [];
+
+    restoreNames(users);
+
+    expect(users).toEqual([]);
+  });
+
+  it('should return firstNames with multiply users', () => {
+    const users = [{
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
+  });
 });
