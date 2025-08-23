9af2cd9b88d9bfac664a12792d0ee69733c75155
Eater228 <543145yura@gmail.com>
2025-08-23 13:40:38 +0300

    Add solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,55 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should restore first names from full names', () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+      {
+        firstName: 'Charlie',
+        lastName: 'Brown',
+        fullName: 'Charlie Brown',
+      },
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+      {
+        firstName: 'Charlie',
+        lastName: 'Brown',
+        fullName: 'Charlie Brown',
+      },
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ]);
   });
-
-  // write tests here
 });
