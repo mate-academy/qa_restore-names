90c89a8d18f80e7aa6673871aced27c8ce26cb13
Robert <robert.szalapak@gmail.com>
2025-09-20 18:55:11 +0200

    adding to required tests

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -3,6 +3,12 @@
 const { restoreNames } = require('./restoreNames');
 
 describe('restoreNames', () => {
+  it('should return undefined', () => {
+    const result = restoreNames([]);
+
+    expect(result).toBeUndefined();
+  });
+
   it(`should restore name if 'users.firstName' equals undefined`, () => {
     const users = [
       {
@@ -68,5 +74,35 @@ describe('restoreNames', () => {
     restoreNames(users);
 
     expect(users).toStrictEqual([]);
+  });
+
+  it(`should restore 'firstName'
+     if there is more than one user in array`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toStrictEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
   });
 });
