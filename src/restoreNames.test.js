5cb29bc12987edbbcd8f2870b11329960d990942
Tomasz Krzywicki <tomaszkrzywicki4@gmail.com>
2025-09-21 20:46:08 +0200

    SolV1

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,60 @@
-'use strict';
+"use strict";
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+const { restoreNames } = require("./restoreNames");
 
-  it('should ', () => {
+describe("restoreNames", () => {
+  it("should restore firstName when it is undefined", () => {
+    const users = [
+      { firstName: undefined, lastName: "Holy", fullName: "Jack Holy" },
+    ];
 
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe("Jack");
   });
 
-  // write tests here
+  it("should restore firstName when it is missing", () => {
+    const users = [{ lastName: "Adams", fullName: "Mike Adams" }];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe("Mike");
+  });
+
+  it("should not change firstName if it already exists", () => {
+    const users = [
+      { firstName: "Sarah", lastName: "Connor", fullName: "Sarah Connor" },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe("Sarah");
+  });
+
+  it("should work for multiple users in array", () => {
+    const users = [
+      { firstName: undefined, lastName: "Holy", fullName: "Jack Holy" },
+      { lastName: "Adams", fullName: "Mike Adams" },
+      { firstName: "Anna", lastName: "Smith", fullName: "Anna Smith" },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      { firstName: "Jack", lastName: "Holy", fullName: "Jack Holy" },
+      { firstName: "Mike", lastName: "Adams", fullName: "Mike Adams" },
+      { firstName: "Anna", lastName: "Smith", fullName: "Anna Smith" },
+    ]);
+  });
+
+  it("should not change lastName or fullName", () => {
+    const users = [
+      { firstName: undefined, lastName: "Brown", fullName: "Tom Brown" },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].lastName).toBe("Brown");
+    expect(users[0].fullName).toBe("Tom Brown");
+  });
 });
