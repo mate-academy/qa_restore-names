28ac3012a33270fc8b6b9ddd0895f9ee02d51ba1
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-12 17:29:39 +0200

    Poprawiona funkcja restoreNames – uzupełnia firstName i lastName
    Funkcja restoreNames przetwarza tablicę obiektów User i uzupełnia brakujące pola firstName oraz lastName na podstawie fullName. Jeśli firstName lub lastName jest pusty, pobiera odpowiednie wyrazy z fullName. Funkcja modyfikuje obiekty bezpośrednio w tablicy i jest zgodna z wymaganiami testów w restoreNames.test.js.

--- src/restoreNames.js
+++ src/restoreNames.js
@@ -10,10 +10,14 @@
  */
 function restoreNames(users) {
   for (const user of users) {
-    if (!user.firstName) {
-      [user.firstName] = user.fullName.split(' ');
+    // jeśli brakuje firstName lub lastName, uzupełniamy z fullName
+    if (!user.firstName || !user.lastName) {
+      const [firstName, lastName] = user.fullName.split(' ');
+      if (!user.firstName) user.firstName = firstName;
+      if (!user.lastName) user.lastName = lastName;
     }
   }
 }
 
 module.exports = { restoreNames };
+
