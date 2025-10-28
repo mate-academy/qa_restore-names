ab0540f0ddc568f1240a90d87ad9a84e36165d89
SerhiiSemennikov <semennik@hotmail.com>
2025-10-28 21:06:57 +0100

    copy1

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,89 @@
-'use strict';
+"use strict";
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+describe("restoreNames", () => {
+  const { restoreNames } = require("./restoreNames");
 
-  it('should ', () => {
+  it("without or undefined firstName ", () => {
+    let users = [
+      {
+        firstName: undefined,
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ];
 
+    users.forEach((user) => {
+      expect(isObject(user)).toBeTruthy();
+    });
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: "Jack",
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ]);
+
+    users.forEach((user) => {
+      expect(isObject(user)).toBeTruthy();
+      expect(user).toHaveProperty("firstName");
+      expect(user).toHaveProperty("lastName");
+      expect(user).toHaveProperty("fullName");
+    });
   });
 
-  // write tests here
+  it("firstName is empty string", () => {
+    let users = [
+      {
+        firstName: "",
+        lastName: "Molly",
+        fullName: "John Molly",
+      },
+      {
+        lastName: "Colins",
+        fullName: "Phil Colins",
+      },
+    ];
+
+    users.forEach((user) => {
+      expect(isObject(user)).toBeTruthy();
+    });
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: "John",
+        lastName: "Molly",
+        fullName: "John Molly",
+      },
+      {
+        firstName: "Phil",
+        lastName: "Colins",
+        fullName: "Phil Colins",
+      },
+    ]);
+
+    users.forEach((user) => {
+      expect(isObject(user)).toBeTruthy();
+      expect(user).toHaveProperty("firstName");
+      expect(user).toHaveProperty("lastName");
+      expect(user).toHaveProperty("fullName");
+    });
+  });
 });
+
+function isObject(value) {
+  return value instanceof Object && value !== null && !Array.isArray(value);
+}
