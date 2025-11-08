6323563b66ff25b54ad41f612253811637400f0a
Mykhailo <kurochkinmy@gmail.com>
2025-11-08 16:46:04 +0100

    feat: add tests for restoreNames function

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,65 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
+  const correctUsers = [
+    {
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    },
+    {
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    },
+  ];
 
-  it('should ', () => {
+  it('should not return anything', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
+    expect(restoreNames(users)).toBe(undefined);
   });
 
-  // write tests here
+  it(`should restore 'firstName' correctly`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual(correctUsers);
+  });
+
+  it(`should restore 'firstName' if it is an empty string`, () => {
+    const usersWithEmptyString = [
+      {
+        firstName: '',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(usersWithEmptyString);
+
+    expect(usersWithEmptyString[0].firstName).toBe('Jack');
+  });
 });
