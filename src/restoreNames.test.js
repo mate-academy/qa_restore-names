9d1f2faf7dd1d0c4ead664602a342ae1d52e087a
Oleg <pryge321@gmail.com>
2025-11-19 12:28:11 +0200

    Added new tests

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-describe("restoreNames", () => {
-  const { restoreNames } = require("./restoreNames");
+describe('restoreNames', () => {
+  const { restoreNames } = require('./restoreNames');
 
-  it("should ", () => {
+  it('should be a function ', () => {
     expect(restoreNames).toBeInstanceOf(Function);
   });
 
@@ -11,26 +11,93 @@ describe("restoreNames", () => {
     const users = [
       {
         firstName: undefined,
-        lastName: "Holy",
-        fullName: "Jack Holy",
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
       },
     ];
 
     restoreNames(users);
 
-    expect(users[0].firstName).toBe("Jack");
+    expect(users[0].firstName).toBe('Jack');
   });
 
   it(`should restore firstname when firstname field is missing`, () => {
     const users = [
       {
-        lastName: "Adams",
-        fullName: "Mike Adams",
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
       },
     ];
 
     restoreNames(users);
 
-    expect(users[0].firstName).toBe("Mike");
+    expect(users[0].firstName).toBe('Mike');
+  });
+
+  it(`function should not return anything`, () => {
+    const users = [
+      {
+        fullName: 'Sarrah Grinch',
+      },
+    ];
+
+    const result = restoreNames(users);
+
+    expect(result).toBeUndefined();
+  });
+
+  it(`should not modigy existing names`, () => {
+    const users = [
+      {
+        firstName: 'Tom',
+        lastName: 'Jerry',
+        fullName: 'Tom Jerry',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Tom');
+    expect(users[0].lastName).toBe('Jerry');
+  });
+
+  it(`should handle multiple users`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Brown',
+        fullName: 'John Brown',
+      },
+      {
+        firstName: undefined,
+        lastName: 'White',
+        fullName: 'Chris White',
+      },
+      {
+        firstName: 'Emily',
+        lastName: 'Davis',
+        fullName: 'Emily Davis',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John',
+        lastName: 'Brown',
+        fullName: 'John Brown',
+      },
+      {
+        firstName: 'Chris',
+        lastName: 'White',
+        fullName: 'Chris White',
+      },
+      {
+        firstName: 'Emily',
+        lastName: 'Davis',
+        fullName: 'Emily Davis',
+      },
+    ]);
   });
 });
