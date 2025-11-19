78b8239fad8367024517f6e136abbd58a988a708
Oleg <pryge321@gmail.com>
2025-11-19 11:54:36 +0200

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,36 @@
-'use strict';
+"use strict";
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+describe("restoreNames", () => {
+  const { restoreNames } = require("./restoreNames");
 
-  it('should ', () => {
-
+  it("should ", () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`should restore name if it is undefined`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe("Jack");
+  });
+
+  it(`should restore firstname when firstname field is missing`, () => {
+    const users = [
+      {
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe("Mike");
+  });
 });
