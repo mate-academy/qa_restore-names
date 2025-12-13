b6733b221a2bddb575c3f5ddacc872c7c386c8f7
Hannah Olbrich <hannaholbrich@gmail.com>
2025-12-13 21:10:32 +0000

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -64,4 +64,36 @@ describe('restoreNames', () => {
       },
     ]);
   });
+
+  it('should return a users lastname if it is undefined', () => {
+    const users = [
+      {
+        firstName: 'John', lastName: undefined, fullName: 'John Lennon',
+      },
+    ];
+
+    const restoredUsers = restoreNames(users);
+
+    expect(restoredUsers).toEqual([
+      {
+        firstName: 'John', lastName: 'Lennon', fullName: 'John Lennon',
+      },
+    ]);
+  });
+
+  it('should handle names with only a first name in fullName', () => {
+    const users = [
+      {
+        firstName: '', lastName: '', fullName: 'Madonna',
+      },
+    ];
+
+    const restoredUsers = restoreNames(users);
+
+    expect(restoredUsers).toEqual([
+      {
+        firstName: 'Madonna', lastName: '', fullName: 'Madonna',
+      },
+    ]);
+  });
 });
