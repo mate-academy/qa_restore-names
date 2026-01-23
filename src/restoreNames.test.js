c758794f89742afd2f653dac474a3328e0e00a1d
Michał Łabucki <labucki.michal@gmail.com>
2026-01-23 16:27:02 +0100

    fix:solution 3

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,49 +1,51 @@
 'use strict';
 
-const { ExportConflictException } = require('@aws-sdk/client-dynamodb');
-
 describe('restoreNames', () => {
   const { restoreNames } = require('./restoreNames');
 
-  it('should set the firstName from fullName when firstName is set as undefined ', () => {
+  it(`should set the firstName from fullName when firstName is undefined `,
+    () => {
+      const users = [
+        {
+          firstName: undefined,
+          lastName: 'Holy',
+          fullName: 'Jack Holy',
+        },
+      ];
+
+      restoreNames(users);
+
+      expect(users[0].firstName).toBe('Jack');
+    });
+
+  it(`should set the firstName from fullName when firstName
+   is not set as property`,
+  () => {
     const users = [
       {
-        firstName: undefined,
         lastName: 'Holy',
         fullName: 'Jack Holy',
       },
-    ]
-    
+    ];
+
     restoreNames(users);
 
-    expect(users[0].firstName).toBe('Jack')
+    expect(users[0].firstName).toBe('Jack');
   });
 
-  it('should set the firstName from fullName when firstName is not set as property  ', () => {
+  it(`should keep the firstName when firstName has a different
+   value than fullName`,
+  () => {
     const users = [
       {
-        lastName: 'Holy',
-        fullName: 'Jack Holy',
-      },
-    ]
-    
-    restoreNames(users);
-
-    expect(users[0].firstName).toBe('Jack')
-  });
-
-  it('should keep the firstName when firstName has a different value than fullName', () => {
-    const users = [
-      { firstName: 'Jack',
+        firstName: 'Jack',
         lastName: 'Holy',
         fullName: 'John Holy',
       },
-    ]
-    
+    ];
+
     restoreNames(users);
 
-    expect(users[0].firstName).toBe('Jack')
+    expect(users[0].firstName).toBe('Jack');
   });
-
-  // write tests here
 });
