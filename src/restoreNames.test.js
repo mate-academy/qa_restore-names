239e3142a6b8356fe5c83b2fb26e1584174d96f9
Radymyr <lobodaradymyr@gmail.com>
2026-01-31 23:59:07 +0200

    fixed

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,35 +1,46 @@
-'use strict';
+"use strict";
 
-describe('restoreNames', () => {
-  const { restoreNames } = require('./restoreNames');
+describe("restoreNames", () => {
+  const { restoreNames } = require("./restoreNames");
 
-  const users = [
-    {
-      firstName: undefined,
-      lastName: 'Holy',
-      fullName: 'Jack Holy',
-    },
-    {
-      lastName: 'Adams',
-      fullName: 'Mike Adams',
-    },
-  ];
-
-  it('should be a function', () => {
+  it("should not return value", () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ];
     const result = restoreNames(users);
 
     expect(result).toBe(undefined);
   });
 
-  it('should be a function', () => {
+  it("should be a function", () => {
     expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  restoreNames(users);
+  it("firstName should be equal first part of fullName", () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ];
 
-  users.forEach((item, index) => {
-    it('firstName should be equl first part of fullName', () => {
-      expect(item.firstName).toBe(item.fullName.split(' ')[0]);
+    restoreNames(users);
+
+    users.forEach((item, index) => {
+      expect(item.firstName).toBe(item.fullName.split(" ")[0]);
     });
   });
 });
