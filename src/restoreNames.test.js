ea21164bb69721dc2afaace4a12420f1a50c7b63
Radymyr <lobodaradymyr@gmail.com>
2026-01-31 22:50:24 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,31 @@
-'use strict';
+"use strict";
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+describe("restoreNames", () => {
+  const { restoreNames } = require("./restoreNames");
 
-  it('should ', () => {
+  const users = [
+    {
+      firstName: undefined,
+      lastName: "Holy",
+      fullName: "Jack Holy",
+    },
+    {
+      lastName: "Adams",
+      fullName: "Mike Adams",
+    },
+  ];
 
+  it("should be a function", () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it("should have firstName", () => {
+    restoreNames(users);
+    expect(users[0].firstName).toBe("Jack");
+  });
+
+  it("should have firstName", () => {
+    restoreNames(users);
+    expect(users[1].firstName).toBe("Mike");
+  });
 });
