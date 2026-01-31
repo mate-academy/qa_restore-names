a0b7134273c4f1c0db68c8b546b227c3122d604b
Radymyr <lobodaradymyr@gmail.com>
2026-01-31 22:58:06 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -19,12 +19,12 @@ describe("restoreNames", () => {
     expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  it("should have firstName", () => {
+  it("if fullName 'Jack Holy' should have firstName 'Jack'", () => {
     restoreNames(users);
     expect(users[0].firstName).toBe("Jack");
   });
 
-  it("should have firstName", () => {
+  it("if fullName 'Mike Adams' should have firstName 'Mike'", () => {
     restoreNames(users);
     expect(users[1].firstName).toBe("Mike");
   });
