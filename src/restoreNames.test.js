50847a8cf2de31ac911e958dde7ae509430d226c
Anton Kuchmasov <antek.ukraine@gmail.com>
2026-02-09 15:50:24 +0200

    added tests for restoreNames function

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,80 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be a function', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should throw error if users is not an array', () => {
+    const users = 'Bob Mike Henry';
+
+    expect(() => {
+      restoreNames(users);
+    }).toThrow();
+  });
+
+  it('should successfully restore firstNames when data is corrupted', () => {
+    const corruptedData = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    const correctData = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(corruptedData);
+
+    expect(corruptedData).toEqual(correctData);
+  });
+
+  it('should don`t affect correct data', () => {
+    const correctUsers = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    const copyOfCorrectUsers = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(correctUsers);
+
+    expect(correctUsers).toEqual(copyOfCorrectUsers);
+  });
 });
