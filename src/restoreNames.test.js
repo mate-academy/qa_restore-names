8505bc1ed29d60b08d9afe97b4b976dec303368d
Vitalii <v.hulaievych@gmail.com>
2026-02-21 15:52:15 +0000

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,90 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should not return anything', () => {
+    const users = [
+  {
+    firstName: undefined,
+    lastName: 'Holy',
+    fullName: 'Jack Holy',
+  },
+  {
+    lastName: 'Adams',
+    fullName: 'Mike Adams',
+  },
+];
 
+    expect(restoreNames(users)).toBeUndefined();
   });
 
-  // write tests here
+  it('should return firstName if it was undefined', () => {
+
+  const users = [
+  {
+    firstName: undefined,
+    lastName: 'Holy',
+    fullName: 'Jack Holy',
+    }]
+
+    restoreNames(users)
+
+    expect(users[0].firstName).toBe('Jack');
+  });
+  it('should restore firstName if it is missing', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Mike');
+  });
+
+  it('should not change users who already have firstName', () => {
+    const users = [
+      {
+        firstName: 'Anna',
+        lastName: 'Smith',
+        fullName: 'Anna Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Anna');
+  });
+
+  it('should correctly update multiple users', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
+  });
 });
