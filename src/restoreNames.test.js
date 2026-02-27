be3a5bf918d8764d497574991a70fe041be41036
Dmytro Nikulin <dmytronikulin@Dmytros-MacBook-Air.local>
2026-02-27 14:11:31 +0200

    Add solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,42 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it(`should restore the first name for entry that
+    is missing the firstName property`, () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users).toBe([{
+      firstName: 'Mike',
+      lastName: 'Adams',
+      fullName: 'Mike Adams',
+    }]);
   });
 
-  // write tests here
+  it(`should restore the first name for entry that
+    has firstName property with 'undefined' value`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toBe([{
+      firstName: 'Jack',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    }]);
+  });
 });
