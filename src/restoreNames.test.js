6b1c8aa929545991e3f1da363a6688cff14e733f
Heorhii Savostikov <georgesavostikov@gmail.com>
2026-03-26 19:46:56 +0200

    Created some tests for the function

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,9 +1,60 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {});
+  it('should add firstName if it is missing', () => {
+    const user = {
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    };
 
-  // write tests here
+    restoreNames([user]);
+
+    expect(user.firstName).toBeDefined();
+  });
+
+  it('should add firstName from fullName when firstName is undefined', () => {
+    const user = {
+      firstName: undefined,
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    };
+
+    restoreNames([user]);
+
+    expect(user.firstName).toBe('Jack');
+  });
+
+  it('should not overwrite existing firstName', () => {
+    const user = {
+      firstName: 'John',
+      lastName: 'Holy',
+      fullName: 'Jack Holy',
+    };
+
+    restoreNames([user]);
+
+    expect(user.firstName).toBe('John');
+  });
+
+  it('should restore firstName for all users in the array', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    for (const user of users) {
+      expect(user.firstName).toBeDefined();
+    }
+  });
 });
