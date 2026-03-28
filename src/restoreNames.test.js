50d5d35c0ea3e25d48a5db7f03297af9b61532bd
Nazarii Kaminskyi <nazarruda333@gmail.com>
2026-03-28 19:56:11 +0200

    Solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,9 +1,67 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {});
+  it('should restore firstName from fullName if firstName is missing', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
-  // write tests here
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
+  });
+
+  it('should not change users that already have firstName', () => {
+    const users = [
+      {
+        firstName: 'Anna',
+        lastName: 'Brown',
+        fullName: 'Kate Brown',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Anna',
+        lastName: 'Brown',
+        fullName: 'Kate Brown',
+      },
+    ]);
+  });
+
+  it('should not return anything', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    const result = restoreNames(users);
+
+    expect(result).toBeUndefined();
+  });
 });
